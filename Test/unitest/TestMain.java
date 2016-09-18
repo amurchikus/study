063078d471b8722bf0600c8e562cf02fd5c5c5a4
Eugen <parasofting@ukr.net>
2016-09-18 15:35:02 +0300

    Added test section

--- Test/unitest/TestMain.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Eugen on 9/18/2016.
- */
-public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        new TestStart().firstTest();
-    }
-}
