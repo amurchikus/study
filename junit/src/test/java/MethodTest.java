086f714a9e938f050986c9b39d4b402206cd796a
Eugen <parasofting@ukr.net>
2016-09-21 13:57:53 +0000

    change test5

--- junit/src/test/java/MethodTest.java
+++ junit/src/test/java/MethodTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class MethodTest extends Main {
     @org.junit.Test
     public void match() throws Exception {
-        new Method().match(a, 5);
+        new Method().match(10, 5);
     }
 
 }
